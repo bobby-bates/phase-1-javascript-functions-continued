a6cd07cf6e9e3be7251cee1ae7023eab53ad59e5
Bobby Bates <robertfbatesjr@gmail.com>
2022-03-27 08:52:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Function declaration:
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+// Function expression:
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+// Using scope chain:
+function wrapAdjective(flair = '*'){
+    return function(adj = 'special'){
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
